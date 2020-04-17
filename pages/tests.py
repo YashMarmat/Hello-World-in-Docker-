c2c75fcd76f48312487b0d3305e398a3b40f3866
Yash Marmat <yashmarmat08@gmail.com>
2020-04-17 17:21:16 -0100

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestUrls(TestCase):
+    
+    def test_home_page_reference_name(self):
+        request = self.client.get(reverse('home'))
+        self.assertEqual(request.status_code, 200)
+
+    def test_home_page_content(self):
+        request = self.client.get(reverse('home'))
+        self.assertContains(request, 'Hello World')
+        
